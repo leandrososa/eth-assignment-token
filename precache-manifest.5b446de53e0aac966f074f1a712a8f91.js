59efbaebfd10fb51ace6e4b37ab284c1231a2acd
leandrososa <52506861+leandrososa@users.noreply.github.com>
2022-03-01 02:23:17 -0300

    Updates

---
+++ precache-manifest.5b446de53e0aac966f074f1a712a8f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70dc909d3188c56ead924a145813f73",
+    "url": "/eth-assignment-token/index.html"
+  },
+  {
+    "revision": "83a625b3221d3eb0ebfc",
+    "url": "/eth-assignment-token/static/css/2.a4e2f9ad.chunk.css"
+  },
+  {
+    "revision": "03eeb5657bb466b92813",
+    "url": "/eth-assignment-token/static/css/main.71f45434.chunk.css"
+  },
+  {
+    "revision": "83a625b3221d3eb0ebfc",
+    "url": "/eth-assignment-token/static/js/2.b928642f.chunk.js"
+  },
+  {
+    "revision": "03eeb5657bb466b92813",
+    "url": "/eth-assignment-token/static/js/main.bc28dea9.chunk.js"
+  },
+  {
+    "revision": "cb88a774e3b1d7b3d314",
+    "url": "/eth-assignment-token/static/js/runtime-main.478f4ae2.js"
+  }
+]);
